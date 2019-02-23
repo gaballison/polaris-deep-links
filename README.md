a616e44233a613a586cc098d11b723bd9ceadd8e
Allison Fredrickson <gaballison@users.noreply.github.com>
2019-02-23 16:24:48 -0500

    Change layout, new color scheme, made JS more responsive to different scenarios

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Polaris Deep Link Generator
 
-This is a tool created for my [Code Louisville](https://codelouisville.org/) front end web development portfolio. I maintain [my library's website](https://jefflibrary.org) and one of my monthly tasks is updating the page listing what all of our [book clubs](https://jefflibrary.org/events/book-clubs) are reading that month. However, one of the frustrating things I've encountered is not being able to copy a link to a specific item directly from our online catalog; the urls are session-specific and so don't work as permanent links.
+This is a tool created for my [Code Louisville](https://codelouisville.org/) front end web development portfolio. I maintain [my library's website](https://jefflibrary.org) and one of my monthly tasks is updating the page listing what all of our [book clubs](https://jefflibrary.org/events/book-clubs) are reading that month. However, one of the frustrating things I've encountered is not being able to copy a link to a specific item directly from [our online catalog](http://jeffersonville.polarislibrary.com); the urls are session-specific and so don't work as permanent links.
 
-The simplest solution I've found so far is to use what Polaris (our integrated library system) calls "deep links" to search results (instead of to specific item records), and these links have to be properly [percent-encoded/URL-encoded](https://www.w3schools.com/tags/ref_urlencode.asp). Doing that manually is tedious so I wanted to create an app that would easily generate a properly encoded URL for me. It also includes a button to test the resulting URL which then also allows me to check the catalog to see if we actually own that item, since sometimes our clubs read books we don't have in our collection.
+The simplest solution I've found so far is to use what our integrated library system, Polaris, calls "deep links" to search results (instead of to specific item records), and these links have to be properly [percent-encoded/URL-encoded](https://www.w3schools.com/tags/ref_urlencode.asp). Doing that manually is tedious so I wanted to create an app that would easily generate a properly encoded URL for me. It also includes a button to test the resulting URL which then also allows me to check the catalog to see if we actually own that item, since sometimes our clubs read books we don't have in our collection.
 
 ## Resources Used
 
@@ -13,15 +13,15 @@ The simplest solution I've found so far is to use what Polaris (our integrated l
 ## Custom CSS Classes
 
 1. ```css
-    .form-wrapper {
-        background: #dedede;
-        padding: .8em;
-        border: solid 1px gainsboro;
-        border-radius: 10px;
-        box-shadow: 0px 2px 5px 0.5px dimgray;
+    footer {
+        width: 100%;
+        margin-top: auto;
+        font-size: .7em;
+        padding: 20px;
+        text-align: center;
     }
     ```
-    Selects the wrapper div with the class `form-wrapper` and styles it with a light grey background, increased padding, slightly rounded edges, and a drop shadow.
+    Selects footer and styles it with a small font size, 20 pixels of padding, and centered font. The `margin-top: auto` makes it sticky because the `body` has uses [Bootstrap's](https://getbootstrap.com) `d-flex flex-column` classes to make it a flexbox layout.
 2. ```css
     #textarea-url {
         font-family: monospace;
@@ -64,20 +64,20 @@ The simplest solution I've found so far is to use what Polaris (our integrated l
             let matches = input.match(/\w*/g);
             let len = matches.length;
 
-            /*  the surname is the last item in the array,               *
-             *  but array always ends in blank space so need length - 2  */
+            /*  the surname is the last item in the array,               
+                but array always ends in blank space so need length - 2  */
             let sur = matches[len - 2];
 
             // format author in Last, First Middle name format
             let author = sur + ",+";
 
-            /*  loop through the rest of the matches   *
-             *  and add + after each name if multiple  */
+            /*  loop through the rest of the matches   
+                and add + after each name if multiple  */
             for (let i = 0; i < len - 2; i++) {
                 if (matches[i] != "") {
 
-                    /*  if the name is the last one before the surname   *
-                     *  don't add a + after it                           */    
+                    /*  if the name is the last one before the surname   
+                        don't add a + after it                           */    
                     if (i == len - 4) {
                         author += matches[i];
                     } else {
@@ -96,4 +96,4 @@ The simplest solution I've found so far is to use what Polaris (our integrated l
 
 ## Future Improvements
 
-Ideally I'd like to return an item count based on the search parameters, but the only way I've seen to do that is using the [Polaris API (PAPI)](http://developer.polarislibrary.com/), which would require some back-end scripting to handle authentication securely. Since I want this to work on Github Pages, back end programs aren't an option for now.
+Ideally I'd like to just return an item count based on the search parameters, but the only way I've seen to do that is using the [Polaris API (PAPI)](http://developer.polarislibrary.com/), which would require some back-end scripting to handle authentication securely. Since I want this to work on Github Pages, back end programs aren't an option for now.
--- js/app.js
+++ js/app.js
@@ -4,117 +4,118 @@ $(document).ready(function () {
   const link = {
     title: "",
     author: "",
-    subject: "",
-    url: "http://jeffersonville.polarislibrary.com/view.aspx?"
+    prefix: "http://jeffersonville.polarislibrary.com/view.aspx?",
+    url: ""
   };
+
 
   /************************
    *      FUNCTIONS       *
    ***********************/
 
-  // Encode the title
+  // FUNCTION to encode the title
   const encodeTitle = inputTitle => {
-    if (inputTitle !== "") {
-      let encoded = encodeURIComponent(inputTitle);
+    let encoded = encodeURIComponent(inputTitle);
 
-      // encodeURIComponent doesn't encode apostrophes, so use .replace method to correct them
-      let apos = encoded.replace(/'/g, "%27");
+    // encodeURIComponent doesn't encode apostrophes, so use .replace method to correct them
+    let apos = encoded.replace(/'/g, "%27");
 
-      // encodeURIComponent replaces spaces with %20 but we need spaces to be +
-      let space = apos.replace(/%20/g, "+");
+    // encodeURIComponent replaces spaces with %20 but we need spaces to be +
+    let space = apos.replace(/%20/g, "+");
 
-      // assign the resulting encoded string to the "title" property of the link object
-      link.title = space;
-    } else {
-      link.title = "";
-    }
-
+    // assign the resulting encoded string to the "title" property of the link object
+    link.title = space;
   };
 
-  // Encode the author
+  // FUNCTION to encode the author
   const encodeAuthor = inputAuthor => {
-    if (inputAuthor !== "") {
-      let input = inputAuthor.trim();
+    let input = inputAuthor.trim();
 
-      // create array of individual words from the input
-      let matches = input.match(/\w*/g);
-      let len = matches.length;
+    // create array of individual words from the input
+    let matches = input.match(/\w*/g);
+    let len = matches.length;
 
-      // the surname is the last item in the array, but array always ends in blank space so need penultimate item instead of last
-      let sur = matches[len - 2];
-      let author = sur + ",+";
+    /*  the surname is the last item in the array,
+        but array always ends in blank space so need length - 2  */
+    let sur = matches[len - 2];
+    let author = sur + ",+";
 
-      // loop through the rest of the matches and add + after each name if multiple
-      for (let i = 0; i < len - 2; i++) {
-        if (matches[i] != "") {
-          if (i == len - 4) {
-            author += matches[i];
-          } else {
-            author += `${matches[i]}+`;
-          }
+    /*  loop through the rest of the matches
+        and add + after each name if multiple  */
+    for (let i = 0; i < len - 2; i++) {
+      if (matches[i] != "") {
+
+        /*  if the name is the last one before the surname
+            don't add a + after it                           */
+        if (i == len - 4) {
+          author += matches[i];
+        } else {
+          author += `${matches[i]}+`;
         }
       }
-      link.author = author;
-    } else {
-      link.author = "";
     }
-
+    link.author = author;
   };
 
-  // Select the textarea so we can copy it to clipboard
-  const selectText = (textarea) => {
+  // FUNCTION to assemble the URL based on the values of the link object
+  const buildURL = () => {
+    let url = "";
+
+    // construct the URL based on existence of title &/or author
+    if (link.title != "" && link.author != "") {
+      url += `${link.prefix}title=${link.title}&author=${link.author}`;
+      $('#btn-submit').prop('disabled', false);
+    } else if (link.title == "" && link.author != "") {
+      url += `${link.prefix}author=${link.author}`;
+      $('#btn-submit').prop('disabled', false);
+    } else if (link.title != "" && link.author == "") {
+      url += `${link.prefix}title=${link.title}`;
+      $('#btn-submit').prop('disabled', false);
+    } else {
+      url = "";
+      $('#btn-submit').prop('disabled', true);
+    }
+    return url;
+  };
+
+  // FUNCTION to assemble the URL based on the values of the link object
+  const buildResults = url => {
+
+    // add the HTML for the constructed URL, including ability to select and copy
+    $('#results').html(`
+            <div class="d-flex justify-content-between">
+                <h3 class="text-muted">Encoded URL</h3> 
+                <span id="copy-status" class="text-success align-self-center"></span> 
+                <button id="btn-copy" class="btn btn-link text-decoration-none"><i class="fas fa-clipboard"></i> Copy</button>
+            </div>
+
+            <textarea class="form-control border border-secondary text-monospace" rows="4" col="10" id="textarea-url">${url}</textarea>
+                
+            <a class="btn btn-primary btn-lg btn-block mt-3" id="btn-visit" href="${url}" target="_blank"><i class="fas fa-book fa-lg"></i> &nbsp; Check Polaris</a>
+        `);
+  };
+
+
+  // FUNCTION to select the textarea so we can copy it to clipboard
+  const selectText = textarea => {
     const input = $(textarea);
     input.focus();
     input.select();
   };
 
-
-  /************************
-  *         EVENTS        *
-  ************************/
-
-  // Check Title on blur
-  $('#title').blur(function(){
-      let title = $('#title').val();
-      encodeTitle(title);
-  });
-
-  // Check Author on blur
-  $('#author').blur(function(){
-    let author = $('#author').val();
-    encodeAuthor(author);
-  });
-
-  // Submitting the form
-  $('#btn-submit').click(function () {
-
-    // construct the URL based on existence of title &/or author
-    let url = "";
-    if (link.title != "" && link.author != "") {
-      url += `${link.url}title=${link.title}&author=${link.author}`;
-    } else if (link.title == "" && link.author != "") {
-      url += `${link.url}author=${link.author}`;
-    } else if (link.title != "" && link.author == "") {
-      url += `${link.url}title=${link.title}`;
-    } else {
-      url = "You haven't entered any data!";
-    }
- 
-    $('#results-url').html(`<h4 class="text-muted">Encoded URL</h4>
-        <textarea class="form-control border border-secondary" rows="5" col="10" id="textarea-url">${url}</textarea>
-        <div class="d-flex justify-content-between mt-3"><button class="btn btn-outline-secondary d-inline-block" id="btn-copy"><i class="fas fa-clipboard"></i> &nbsp; Copy link</button><span id="copy-status" class="text-success align-self-center"></span><a class="btn btn-outline-primary d-inline-block" id="btn-visit" href="${url}" target="_blank"><i class="fas fa-sign-out-alt"></i> &nbsp; Check Polaris</a></div>`);
-
-    // see if it will copy properly
+  // FUNCTION to copy URL to clipboard and set statuses accordingly
+  const tryCopy = () => {
     try {
       $('#btn-copy').click(function () {
         selectText('#textarea-url');
         document.execCommand("copy");
-        $('#textarea-url').addClass('border border-success');
-        $('#btn-copy').removeClass('btn-outline-secondary').addClass('btn-outline-success');
+        $('#textarea-url').removeClass('border-secondary').addClass('border-success');
+        $('#btn-copy').addClass('text-success');
         $('#copy-status').html(`<i class="fas fa-check-circle fa-lg"></i> Copied!`);
+
         $('#textarea-url').blur(function () {
-          $(this).removeClass('border-success').addClass('border-secondary');
-          $('#btn-copy').removeClass('btn-outline-success').addClass('btn-outline-secondary');
+          $('#textarea-url').removeClass('border-success').addClass('border-secondary');
+          $('#btn-copy').removeClass('text-success').addClass('text-secondary');
           $('#copy-status').html("");
         });
       });
@@ -125,15 +126,73 @@ $(document).ready(function () {
       $('#copy-status').removeClass('text-success').addClass('text-danger').html(`<i class="fas fa-times-circle fa-lg"></i> ERROR`);
       console.error("Ooops, unable to copy and select. " + error);
     }
+  };
 
+
+  /************************
+  *         EVENTS        *
+  ************************/
+
+  // Check Title on blur, keyup, or change
+  $('#title').on({
+    'blur keyup': function () {
+      let title = $('#title').val();
+      if (title !== "") {
+        encodeTitle(title);
+        $('#btn-submit').prop('disabled', false);
+      } else {
+        if ($('#author').val() !== "") {
+          $('#btn-submit').prop('disabled', false);
+        } else {
+          $('#btn-submit').prop('disabled', true);
+        }
+      }
+    }, change: function () {
+      encodeTitle($('#title').val());
+    }
+  });
+
+  // Check Author on blur, keyup, or change
+  $('#author').on({
+    'blur keyup': function () {
+      let author = $('#author').val();
+      if (author !== "") {
+        encodeAuthor(author);
+        $('#btn-submit').prop('disabled', false);
+      } else {
+        if ($('#title').val() !== "") {
+          $('#btn-submit').prop('disabled', false);
+        } else {
+          $('#btn-submit').prop('disabled', true);
+        }
+      }
+    }, change: function () {
+      encodeAuthor($('#author').val());
+    }
+  });
+
+  // Prevent default submit event
+  $('#form-search').submit(function (event) {
+    let url = buildURL();
+    buildResults(url);
+    tryCopy();
+    event.preventDefault();
+  });
+
+  // Submitting the form
+  $('#btn-submit').click(function () {
+    let url = buildURL();
+    buildResults(url);
+    tryCopy();
   });
 
   // Clear the form and delete all of the results HTML
-  $('#btn-clear').click(function () {
-    $("#results-url").html("");
-    $("#preview").html("");
+  $('#btn-clear').click(function (event) {
+    $('#results').empty();
+    event.preventDefault();
     $(':text').val('');
+    $('#btn-submit').prop('disabled', true);
+    link.author = "";
+    link.title = "";
   });
-
-
 });
